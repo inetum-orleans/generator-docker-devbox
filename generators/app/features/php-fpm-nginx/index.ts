b82be5cf119f1aa23dbfee09ed52c78c08e53b62
Rémi Alvergnat <toilal.dev@gmail.com>
2018-11-28 22:06:47 +0100

    Add version question for nginx container

--- generators/app/features/php-fpm-nginx/index.ts
+++ generators/app/features/php-fpm-nginx/index.ts
@@ -4,14 +4,38 @@ import { FeatureContext } from '../../index'
 import { PortsManager } from '../../managers'
 import { Php } from '../common/php'
 import { DockerDevboxExt } from '../../docker'
+import { RegistryClient } from '../../docker/registry'
 
 export class PhpFpmNginx extends Php implements DockerComposeFeature<PhpFpmNginx>, FeatureAsyncInit {
   name: string = 'php-fpm-nginx'
-  label: string = 'Nginx with PHP-FPM'
+  label: string = 'NGINX with PHP-FPM'
   instanceName: string = 'php'
   otherInstanceNames: string[] = ['web']
   directory: string | string[] = [dirnameFrom('php'), __dirname]
   phpMode: string = 'fpm'
+
+  async initAsync () {
+    await super.initAsync()
+
+    const registry = new RegistryClient()
+    const nginxTax = await registry.tagsList('nginx')
+
+    const tags = nginxTax
+      .filter(tag => /^\d+\.\d+(?:.\d+)?$/.test(tag) || tag === 'stable' || tag === 'mainline')
+      .reverse()
+
+    this.asyncQuestions = [
+      ...this.asyncQuestions,
+      {
+        type: 'list',
+        name: 'nginxVersion',
+        message: 'NGINX version',
+        choices: tags,
+        default: 'stable',
+        store: true
+      }
+    ]
+  }
 
   dockerComposeConfiguration (builder: ConfigBuilder, context: FeatureContext<PhpFpmNginx>, portsManager: PortsManager, dev?: boolean): void {
     super.dockerComposeConfiguration(builder, context, portsManager)
